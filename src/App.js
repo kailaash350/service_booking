0fe4a2f265d628c6651cf87df3391fa7aa03796a
kailaash350 <43023881+kailaash350@users.noreply.github.com>
2021-08-29 20:39:18 +1000

    home layout

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Header from './Components/Header';
+import { Login } from './Components/Login';
+import bgImage from '/Users/orion/Desktop/service_booking/src/images/backG_SrBK.png';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="application-page">
+      <Header/>
+      <img className="bg-img" src={bgImage} alt="bg" />
+      <Login/>
     </div>
   );
 }
---
+++ src/Components/Login.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const Login = () => {
+    return (
+        <div className="login-container">
+            <h1>Login Page</h1>
+        </div>
+    )
+}
